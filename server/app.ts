d4a1d15e358ff160645e5e2eda246819a44aa41d
Nikolay Dimitrov <n.zhdimitrov@gmail.com>
2024-08-05 19:06:37 +0300

    modified database
    Moved database from local host to cloud server

--- server/app.ts
+++ server/app.ts
@@ -4,7 +4,6 @@ import cors from "cors";
 import {userRouter} from "./routes/user.router";
 
 const app: Application = express();
-const port = 3001;
 
 app.use(cors());
 app.use(json());
@@ -13,8 +12,4 @@ app.use(userRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
 });
--- server/core/DB.ts
+++ server/core/DB.ts
@@ -1,13 +1,16 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 export class DB {
     conn;
 
     constructor() {
         this.conn = mysql.createPool({
-            host: 'localhost',
-            user: 'root',
-            database: "car_dealer",
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
         }).promise();
     }
 }
