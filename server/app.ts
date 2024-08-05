ea8a6d0cfccbdfc363be7275cd851f339c8a0815
Nikolay Dimitrov <n.zhdimitrov@gmail.com>
2024-08-05 22:15:29 +0300

    revert port

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import {userRouter} from "./routes/user.router";
 
 const app: Application = express();
+const port = 3001;
 
 app.use(cors());
 app.use(json());
@@ -12,4 +13,8 @@ app.use(userRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
