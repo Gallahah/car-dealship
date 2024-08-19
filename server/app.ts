2c4cac0041a8ac5831a0ea93adc95e582e28dcf6
Nikolay Dimitrov <n.zhdimitrov@gmail.com>
2024-08-19 18:09:53 +0300

    build

--- server/app.ts
+++ server/app.ts
@@ -11,9 +11,10 @@ const app: Application = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.NODE_ENV === 'production' ? 'https://your-production-domain.com' : 'http://localhost:5173',
     credentials: true,
 }));
+
 
 app.use(json());
 app.use(cookieParser());
